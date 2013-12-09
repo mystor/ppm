1a8bf46db54b9be34dd6b1166a77bdbbc1d36d17
Michael Layzell <michael@redrasor.com>
2013-12-09 16:55:14 -0500

    First Version

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ppm',
+      version='0.1',
+      description='NPM-Inspired Python Project Manager',
+      author='Michael Layzell',
+      author_email='michael@thelayzells.com',
+      url='https://github.com/Mystor/ppm',
+      scripts=['ppm'])
